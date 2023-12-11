0216eb47a3449d19c7aa40f9c55e8f8bc60dc159
Nikita.Rudy <NVRudy@gmail.com>
2023-12-11 18:53:30 +0300

    oop 2.2 part 1

---
+++ src/Gryffindor.java
@@ -0,0 +1,62 @@
+public class Gryffindor extends Hogwarts {
+
+    private int nobility;//благородство
+    private int honor;//честь
+    private int bravery;//храбрость
+
+    public Gryffindor (String name,int conjure, int transgress, int nobility, int honor, int bravery) {
+        super(name,conjure, transgress);
+
+        this.nobility = nobility;
+        this.honor = honor;
+        this.bravery = bravery;
+    }
+    public Gryffindor (){}
+
+    public int getNobility() {
+        return nobility;
+    }
+
+    public void setNobility(int nobility) {
+        this.nobility = nobility;
+    }
+
+    public int getHonor() {
+        return honor;
+    }
+
+    public void setHonor(int honor) {
+        this.honor = honor;
+    }
+
+    public int getBravery() {
+        return bravery;
+    }
+
+    public void setBravery(int bravery) {
+        this.bravery = bravery;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Студент факультета Гриффиндор " +getName()+
+                ": навык колдовать = "+getConjure()+
+                ", навык трансгрессии = "+getTransgress()+
+                ", благородство = " + nobility +
+                ", честь = " + honor +
+                ", храбрость = " + bravery
+                ;
+    }
+
+
+    public void printCompareFacult(Gryffindor hogwarts2){
+        if((nobility + honor+bravery) > (hogwarts2.nobility + hogwarts2.bravery+hogwarts2.honor))
+        { System.out.println(getName() + " лучший Гриффиндорец, чем " +
+                hogwarts2.getName());}
+        else
+        {System.out.println(hogwarts2.getName() + " лучший Гриффиндорец, чем  " +
+                getName());}
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+    public  class Main {
+
+
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+
+            Gryffindor hogwarts1 = new Gryffindor("Гарри Поттер", 56, 72, 88, 65, 76);
+            Gryffindor hogwarts2 = new Gryffindor("Гермиона Грейнджер", 44, 35, 34, 28, 43);
+            Gryffindor hogwarts3 = new Gryffindor("Рон Уилзи", 47, 35, 43, 28, 43);
+            Hufflepuff hogwarts4 = new Hufflepuff("Захария Смит",22,33,54,48,32);
+            Hufflepuff hogwarts5 = new Hufflepuff("Седрик Диггори", 27, 31, 59, 47, 52);
+            Hufflepuff hogwarts6 = new Hufflepuff("Джастин Финч-Флетчли", 29, 40, 64, 35, 28);
+            Ravenclaw hogwarts7 = new Ravenclaw("Чжоу Чанг", 54, 12, 47, 65, 54, 55);
+            Ravenclaw hogwarts8 = new Ravenclaw("Падма Патил", 43, 28, 24, 55, 54, 66);
+            Ravenclaw hogwarts9 = new Ravenclaw("Маркус Белби", 65, 21, 76, 39, 53, 43);
+            Slytherin hogwarts10 = new Slytherin("Драко Малфой", 32, 37, 44, 36, 26, 53, 22);
+            Slytherin hogwarts11 = new Slytherin("Грэхэм Монтегю", 27, 34, 45, 34, 44, 25, 41);
+            Slytherin hogwarts12 = new Slytherin("Грегори Гойл", 22, 33, 33, 21, 25, 42, 66);
+
+            hogwarts1.printCompare(hogwarts2);
+            hogwarts5.printCompare(hogwarts8);
+            System.out.println(hogwarts2.toString());
+            System.out.println(hogwarts5.toString());
+            System.out.println(hogwarts8.toString());
+            System.out.println(hogwarts11.toString());
+            hogwarts1.printCompareFacult(hogwarts3);
+            hogwarts5.printCompareFacult(hogwarts6);
+            hogwarts7.printCompareFacult(hogwarts8);
+            hogwarts11.printCompareFacult(hogwarts12);
 
         }
     }
